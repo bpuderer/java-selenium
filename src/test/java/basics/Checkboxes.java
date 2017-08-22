0ad63cde63776fd97ff2391057caa18ca7bfb16e
Ben Puderer <bpuderer@yahoo.com>
2017-08-22 00:08:39 -0400

    checkboxes

---
+++ src/test/java/basics/Checkboxes.java
@@ -0,0 +1,25 @@
+package basics;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkboxes {
+
+	@Test
+	public void testCheckbox() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com/");
+
+		WebElement checkbox = driver.findElement(By.cssSelector("#rememberUn"));
+		Assert.assertEquals(false, checkbox.isSelected());
+		checkbox.click();
+		Assert.assertEquals(true, checkbox.isSelected());
+		
+		driver.quit();
+	}
+
+}
--- src/test/java/basics/Dropdowns.java
+++ src/test/java/basics/Dropdowns.java
@@ -1,14 +1,12 @@
 package basics;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class Dropdowns {
-
 
 	@Test
 	public void testStaticDropdown() {
@@ -24,6 +22,5 @@ public class Dropdowns {
 
 		driver.quit();
 	}
-
 
 }
