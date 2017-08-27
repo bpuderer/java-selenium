f015cbe39d449d5f2e123c246bb2ec13754ff1f7
Ben Puderer <bpuderer@yahoo.com>
2017-08-27 19:56:25 -0400

    explicit and implicit wait

--- src/test/java/basics/DisplayedEnabled.java
+++ src/test/java/basics/DisplayedEnabled.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import junit.framework.Assert;
 
 public class DisplayedEnabled {
 
